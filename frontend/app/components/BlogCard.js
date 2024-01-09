89bb18cc87dc5116ec1e14d9a903317532e0e5f4
yelsy <yelsycg17@gmail.com>
2024-01-09 18:31:15 -0500

    add blog with static site

--- frontend/app/components/BlogCard.js
+++ frontend/app/components/BlogCard.js
@@ -1,12 +1,16 @@
+import Link from "next/link";
 
-const BlogCard = ({ title, imageUrl,id }) => {
+//components/BlogCard
+const BlogCard = ({ title, imageUrl, contentn, blogId }) => {
   return (
+    <Link href={`/blog/${blogId}`}>
     <div className="border border-[#2E4053] bg-[#EAFAF1] rounded-lg bg-rounded-lg p-4 shadow-md m-5">
       <img src={imageUrl} alt={title} className="w-full h-40 object-cover" />
       <div className="p-4">
         <h2 className="text-xl font-bold text-center mb-2">{title}</h2>
       </div>
     </div>
+    </Link>
   );
 };
 
--- frontend/app/components/HomePage.js
+++ frontend/app/components/HomePage.js
@@ -3,7 +3,7 @@ const Body = () => {
     <div className="flex flex-col lg:flex-row min-h-screen bg-white text-white p-0 m-0">
       <div className="container mx-auto pt-[20px]">
         <div className="lg:flex">
-          <div className="lg:w-[60%] text-black mt-[40px] pb-[40px]">
+          <div className="lg:w-[60%] text-black mt-[40px] pb-[40px] prose">
             <h1 className="text-[48px] leading-[58px] font-bold mb-4">
               Mision
             </h1>
